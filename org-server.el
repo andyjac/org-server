8817ec22b11a009eb326d85e9afcb28c17d75675
Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
2022-12-02 09:56:23 -0800

    chore: initial commit

---
+++ org-server.el
@@ -0,0 +1,25 @@
+;;; org-server.el --- Server for org properties -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Andrew Jacobson
+;;
+;; Author: Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
+;; Maintainer: Andrew Jacobson <andrew.isaac.jacobson@gmail.com>
+;; Created: December 02, 2022
+;; Modified: December 02, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/andrew/org-server
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;; TODO: Add commentary
+;;
+;;
+;;; Code:
+
+(message "hello from 'org-server.el'")
+
+(provide 'org-server)
+;;; org-server.el ends here
